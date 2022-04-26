40f543e6bcade5bf5dc4bbbf84e58d4a92d457f6
Brandon Pang <brandon.pang@geniusorc.com>
2022-04-26 16:02:30 -0700

    add css module for next

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import NavBar from "../components/NavBar";
+
+export default function About(){
+    return (
+         <>
+        <NavBar/>
+        <div>
+            <h1>About us</h1>
+        </div>
+        </>
+    )
+}
